8f9c31770c70de55d00626a552788079ec8a0b9d
Renato Bezerra <rfbezerra@gmail.com>
2019-05-23 19:25:15 -0300

    chore: Add choice to example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,33 +3,34 @@ import { hot } from 'react-hot-loader/root';
 import * as Yup from 'yup';
 
 import {
- Form, Input, Select, Scope, SubmitHandler, Choice,
+ Form, Input, Scope, SubmitHandler, Choice,
 } from '../../lib';
 import DatePicker from './components/DatePicker';
 import ReactSelect from './components/ReactSelect';
 
 const schema = Yup.object().shape({
-  // name: Yup.string().required(),
-  // profile: Yup.string().required(),
-  // tech: Yup.string().required(),
-  // date: Yup.date().required(),
-  // people: Yup.array(Yup.string())
-  //   .min(2)
-  //   .required(),
-  // billingAddress: Yup.object().shape({
-  //   street: Yup.string().required(),
-  //   number: Yup.string().required()
-  // }),
-  // shippingAddress: Yup.object().when("$useShippingAsBilling", {
-  //   is: false,
-  //   then: Yup.object().shape({
-  //     street: Yup.string().required(),
-  //     number: Yup.string().required()
-  //   }),
-  //   otherwise: Yup.object().strip(true)
-  // }),
-  teste: Yup.array(Yup.string()),
-  teste2: Yup.string().required(),
+  name: Yup.string().required(),
+  profile: Yup.string().required(),
+  tech: Yup.string().required(),
+  date: Yup.date().required(),
+  people: Yup.array(Yup.string())
+    .min(2)
+    .required(),
+  billingAddress: Yup.object().shape({
+    street: Yup.string().required(),
+    number: Yup.string().required(),
+  }),
+  shippingAddress: Yup.object().when('$useShippingAsBilling', {
+    is: false,
+    then: Yup.object().shape({
+      street: Yup.string().required(),
+      number: Yup.string().required(),
+    }),
+    otherwise: Yup.object().strip(true),
+  }),
+  choice: Yup.array(Yup.string()),
+  choice2: Yup.string().required(),
+  choice3: Yup.string().required(),
 });
 
 interface Data {
@@ -41,7 +42,7 @@ interface Data {
   billingAddress: {
     number: number;
   };
-  teste?: string[];
+  choice?: string[];
 }
 
 function App() {
@@ -56,7 +57,7 @@ function App() {
     billingAddress: {
       number: 833,
     },
-    teste: ['1', '3'],
+    choice: ['1', '3'],
   });
 
   const handleSubmit: SubmitHandler<Data> = (data, { resetForm }) => {
@@ -72,28 +73,6 @@ function App() {
       schema={schema}
       onSubmit={handleSubmit}
     >
-      <Choice
-        name="teste"
-        options={[
-          { id: '1', label: 'Um' },
-          { id: '2', label: 'Dois' },
-          { id: '3', label: 'Tres' },
-          { id: '4', label: 'Quatro' },
-        ]}
-        multiple
-      />
-      <br />
-      <Choice
-        name="teste2"
-        options={[
-          { id: '1', label: 'Um' },
-          { id: '2', label: 'Dois' },
-          { id: '3', label: 'Tres' },
-          { id: '4', label: 'Quatro' },
-        ]}
-      />
-      <br />
-      {/*
       <Input name="name" label="Nome" />
       <Input multiline name="profile" label="Perfil" />
 
@@ -136,7 +115,32 @@ function App() {
         <Input name="street" />
         <Input name="number" />
       </Scope>
-      */}
+
+      <br />
+
+      <Choice
+        name="choice"
+        options={[
+          { id: '1', label: 'Um' },
+          { id: '2', label: 'Dois' },
+          { id: '3', label: 'Tres' },
+          { id: '4', label: 'Quatro' },
+        ]}
+        multiple
+      />
+      <br />
+      <Choice
+        name="choice2"
+        options={[
+          { id: '1', label: 'Um' },
+          { id: '2', label: 'Dois' },
+          { id: '3', label: 'Tres' },
+          { id: '4', label: 'Quatro' },
+        ]}
+      />
+      <br />
+      <Choice name="choice3" options={[{ id: 'sim', label: 'Aceito os termos...' }]} />
+      <br />
 
       <button type="submit">Enviar</button>
     </Form>
