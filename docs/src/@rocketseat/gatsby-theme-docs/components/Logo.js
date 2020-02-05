10bbfe77e62262c3ad90d12222a97d44a83d05fd
Diego Fernandes <diego.schell.f@gmail.com>
2020-02-05 19:12:55 -0300

    docs: Update README

--- docs/src/@rocketseat/gatsby-theme-docs/components/Logo.js
+++ docs/src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -3,8 +3,8 @@ import styled from '@emotion/styled';
 import logo from '../../../assets/unform.svg';
 
 export default styled.div`
-  width: 145px;
-  height: 44px;
+  width: 165px;
+  height: 32px;
   background-size: contain;
   background: url(${logo}) left no-repeat;
 `;
