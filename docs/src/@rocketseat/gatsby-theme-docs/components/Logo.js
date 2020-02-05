cc525648bc1c1ebcf02f7ac1911f1ed716ce6d72
Diego Fernandes <diego.schell.f@gmail.com>
2020-02-05 18:48:58 -0300

    docs: First version of docs

---
+++ docs/src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+import logo from '../../../assets/unform.svg';
+
+export default styled.div`
+  width: 145px;
+  height: 44px;
+  background-size: contain;
+  background: url(${logo}) left no-repeat;
+`;
