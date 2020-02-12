9427b817f64f404ae4b31d4f08d1b109bd74066c
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2020-02-12 20:21:35 -0300

    chore: Remove typings and coverage folders

--- packages/core/typings/Scope.d.ts
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react';
-export interface ScopeProps {
-    path: string;
-    children: ReactNode;
-}
-export default function Scope({ path, children }: ScopeProps): JSX.Element;
-//# sourceMappingURL=Scope.d.ts.map
--- packages/core/typings/useField.d.ts
+++
@@ -1,7 +0,0 @@
-export default function useField(name: string): {
-    fieldName: string;
-    registerField: (field: import("./types").UnformField) => void;
-    defaultValue: any;
-    error: string;
-};
-//# sourceMappingURL=useField.d.ts.map
