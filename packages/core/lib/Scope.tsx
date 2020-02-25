52d9564abcb8d2a7857226ed1ba6ae2a325c74d8
Diego Fernandes <diego.schell.f@gmail.com>
2020-02-25 14:11:44 -0300

    ci: Test CI

--- packages/core/lib/Scope.tsx
+++ packages/core/lib/Scope.tsx
@@ -11,13 +11,15 @@ export default function Scope({ path, children }: ScopeProps) {
   const { scopePath, ...form } = useContext(FormContext);
 
   return (
-    <FormContext.Provider
-      value={{
-        ...form,
-        scopePath: scopePath.concat(scopePath ? `.${path}` : path),
-      }}
-    >
-      {children}
-    </FormContext.Provider>
+    <>
+      <FormContext.Provider
+        value={{
+          ...form,
+          scopePath: scopePath.concat(scopePath ? `.${path}` : path),
+        }}
+      >
+        {children}
+      </FormContext.Provider>
+    </>
   );
 }
